e582031cd26a9e122cf97c55cea7f042266dd843
Andy Lok <andylokandy@hotmail.com>
2024-01-22 11:11:14 +0800

    refactor: StringType::Scalar: `Vec<u8>` -> `String` (#14349)
    * refactor: StringType::Scalar: Vec<u8> -> String
    
    * fix substr, split, split_part
    
    * fix storage convert to string
    
    * fix build
    
    * fix concat concat_ws
    
    * fix variant functions
    
    * fix stack overflow
    
    * migrate regexp functions
    
    * migrate 4 string function
    
    * fix replace
    
    * refactor: migrate some string funcs (#14363)
    
    migrate some string funcs
    
    * fix replace
    
    * fix string functions
    
    * fix like, quote, instr
    
    * fix
    
    * fix domain
    
    * optimize reverse
    
    * optimize reverse
    
    * improve to_bitmap
    
    * fmt
    
    * fix build
    
    * add more
    
    * fix strcmp
    
    * fix clippy
    
    * fix
    
    * fix domain
    
    * fix regexp_replace
    
    * fix ascii
    
    * fix regexp_instr
    
    * simplify string domain
    
    * add utf8 string tests
    
    * fix clippy
    
    * Fix scalar tuple
    
    * Fix task dependency error
    
    ---------
    
    Co-authored-by: baishen <baishen2009@gmail.com>
    Co-authored-by: jw <freejw@gmail.com>
    Co-authored-by: sundy-li <543950155@qq.com>
    Co-authored-by: Yijun Zhao <ariesdevil77@gmail.com>

--- src/query/service/src/servers/http/v1/query/execute_state.rs
+++ src/query/service/src/servers/http/v1/query/execute_state.rs
@@ -323,7 +323,7 @@ async fn execute(
         // duplicate codes, but there is an async call
         let data = BlockEntry::new(
             DataType::String,
-            databend_common_expression::Value::Scalar(Scalar::String(err.to_string().into_bytes())),
+            databend_common_expression::Value::Scalar(Scalar::String(err.to_string())),
         );
         block_sender.send(DataBlock::new(vec![data], 1), 1).await;
         return Err(err);
@@ -340,9 +340,7 @@ async fn execute(
             // duplicate codes, but there is an async call
             let data = BlockEntry::new(
                 DataType::String,
-                databend_common_expression::Value::Scalar(Scalar::String(
-                    err.to_string().into_bytes(),
-                )),
+                databend_common_expression::Value::Scalar(Scalar::String(err.to_string())),
             );
             block_sender.send(DataBlock::new(vec![data], 1), 1).await;
             Executor::stop(&executor, Err(err), false).await;
@@ -361,7 +359,7 @@ async fn execute(
                         let data = BlockEntry::new(
                             DataType::String,
                             databend_common_expression::Value::Scalar(Scalar::String(
-                                err.to_string().into_bytes(),
+                                err.to_string(),
                             )),
                         );
                         block_sender.send(DataBlock::new(vec![data], 1), 1).await;
